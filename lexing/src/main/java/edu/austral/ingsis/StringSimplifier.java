a250061bee33e1ec875e44cb12c39b7ad3340ff0
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2021-03-25 00:09:58 -0300

    string delimiters are no longer static

--- lexing/src/main/java/edu/austral/ingsis/StringSimplifier.java
+++ lexing/src/main/java/edu/austral/ingsis/StringSimplifier.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,39 +14,30 @@ public class StringSimplifier {
 
     public static List<Line> removeEnters(String s){
         List<Line> result = new ArrayList<>();
+        List<String> stringDelimiters = getStringDelimiters();
         AtomicInteger row = new AtomicInteger(1);
         String nonChecked = s;
-        while (nonChecked.contains("\"") || nonChecked.contains("'")) {
+        while (stringDelimiters.stream().anyMatch(nonChecked::contains)) {
             String between = getFirstStringInText(nonChecked);
             String[] aux = split(nonChecked, between).toArray(String[]::new);
             nonChecked = aux.length > 1 ? aux[1] : "";
-            if(!result.isEmpty()){
-                row.decrementAndGet();
-                List<Line> newLines = Arrays.stream(aux[0].split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList());
-                mergeLists(result, newLines);
-            }
-            else {
-                result = Arrays.stream(aux[0].split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList());
-            }
-            if(aux[0].charAt(aux[0].length()-1) == '\n') {
-                result.add(new Line(between, row.getAndIncrement()));
-            } else {
-                result.set(result.size()-1, result.get(result.size()-1).concatText(between));
-            }
+
+            if(!result.isEmpty()) addNewAndMerge(result, aux[0], row);
+            else result = Arrays.stream(aux[0].split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList());
+
+            if(aux[0].charAt(aux[0].length()-1) == '\n') result.add(new Line(between, row.getAndIncrement()));
+            else result.set(result.size()-1, result.get(result.size()-1).concatText(between));
         }
         if (nonChecked.equals(s)) result.addAll(Arrays.stream(s.split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList()));
-        else{
-            row.decrementAndGet();
-            List<Line> last = Arrays.stream(nonChecked.split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList());
-            mergeLists(result, last);
-        }
+        else addNewAndMerge(result, nonChecked, row);
         return result;
     }
 
     public static List<String> removeSpaces(String s){
         List<String> split = new ArrayList<>();
+        List<String> stringDelimiters = getStringDelimiters();
         String nonChecked = s;
-        while (nonChecked.contains("\"") || nonChecked.contains("'")) {
+        while (stringDelimiters.stream().anyMatch(nonChecked::contains)) {
             String between = getFirstStringInText(nonChecked);
             String[] aux = split(nonChecked, between).toArray(String[]::new);
             nonChecked = aux.length > 1 ? aux[1] : "";
@@ -57,6 +49,16 @@ public class StringSimplifier {
         return split;
     }
 
+    private static List<String> getStringDelimiters(){
+        return StringUtils.substringBetween(KeyWord.STRING.getRegex(), "[", "]")
+                .chars().boxed().map(Character::toString).collect(toList());
+    }
+
+    private static void addNewAndMerge(List<Line> linesSoFar, String toAdd, AtomicInteger row){
+        row.decrementAndGet();
+        List<Line> newLines = Arrays.stream(toAdd.split("\n")).map(text -> new Line(text, row.getAndIncrement())).collect(toList());
+        mergeLists(linesSoFar, newLines);
+    }
 
     private static void mergeLists(List<Line> oldLines,List<Line> newLines){
         oldLines.set(oldLines.size()-1, oldLines.get(oldLines.size()-1).concatText(newLines.get(0).getText()));
@@ -64,31 +66,29 @@ public class StringSimplifier {
     }
 
     private static List<String> split(String txt, String value) {
+        char reference = value.charAt(0);
         List<String> array = new ArrayList<>();
         if (txt.contains(value)) {
-            array.add(txt.substring(0, txt.indexOf(value.charAt(0))));
-            String s = txt.substring(txt.indexOf(value.charAt(0))+1);
-            if(!s.substring(s.indexOf(value.charAt(0))+1).isEmpty()) array.add(s.substring(s.indexOf(value.charAt(0))+1));
-        } else {
-            array.add(value);
-        }
+            array.add(txt.substring(0, txt.indexOf(reference)));
+            String s = txt.substring(txt.indexOf(reference)+1);
+            String next = s.substring(s.indexOf(reference)+1);
+            if(!next.isEmpty()) array.add(next);
+        } else array.add(value);
         return array;
     }
 
     private static String getFirstStringInText(String text){
         String colons = whichCharacterAppearsFirst(text);
         String subBetween =  StringUtils.substringBetween(text, colons);
-        if (subBetween == null) {
-            throw new RuntimeException("Missing one " + colons + " in " + text);
-        }
+        if (subBetween == null) throw new RuntimeException("Missing one " + colons + " in " + text);
         return colons + subBetween + colons;
     }
 
     private static String whichCharacterAppearsFirst(String text){
-        int one = text.indexOf('\"');
-        int two = text.indexOf('\'');
-        if(one == -1) return "'";
-        if(two == -1) return "\"";
-        return one < two ? "\"" : "'";
+        char[] chars = StringUtils.substringBetween(KeyWord.STRING.getRegex(), "[", "]").toCharArray();
+        List<Integer> indexes = String.copyValueOf(chars).chars().map(text::indexOf)
+                .map(i -> i == -1 ? Integer.MAX_VALUE : i).boxed().collect(toList());
+        int indexOfNearest = indexes.indexOf(Collections.min(indexes));
+        return String.valueOf(chars[indexOfNearest]);
     }
 }
