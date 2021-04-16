21b3a4a3bc98d4bf43862754479c73881031df7f
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-04-16 17:20:30 -0300

    changes

--- utils/src/test/java/edu/austral/ingsis/VariableTest.java
+++ utils/src/test/java/edu/austral/ingsis/VariableTest.java
@@ -12,12 +12,12 @@ public class VariableTest {
     Variable expected = new ConcreteVariable("x", type);
     expected.setValue("hola");
 
-    Variable actual = builder.setName("x").setType(type).setValue("hola").build();
+    Variable actual = builder.withName("x").withType(type).withValue("hola").build();
     Assertions.assertEquals(expected.getName(), actual.getName());
     Assertions.assertEquals(expected.getType(), actual.getType());
     Assertions.assertEquals(expected.getValue(), actual.getValue());
 
-    Variable newActual = builder.setType("string", 3).build();
+    Variable newActual = builder.withType("string", 3).build();
     Assertions.assertEquals(type.getName(), newActual.getType().getName());
     Assertions.assertEquals(type.getOrdinal(), newActual.getType().getOrdinal());
   }
