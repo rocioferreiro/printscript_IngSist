5d1f9914958f444a214de129d257924de49b20bb
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-04-21 21:39:05 -0300

    new version fixed

--- interpreting/src/main/java/edu/austral/ingsis/ConcreteInterpreter.java
+++ interpreting/src/main/java/edu/austral/ingsis/ConcreteInterpreter.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ConcreteInterpreter implements Interpreter {
 
