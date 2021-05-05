c8a22da441a4db0bedc24108a67a62423085c4c3
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2021-05-05 18:26:48 -0300

    try hooks

--- parsing/src/main/java/edu/austral/ingsis/Parser.java
+++ parsing/src/main/java/edu/austral/ingsis/Parser.java
@@ -2,7 +2,5 @@ package edu.austral.ingsis;
 
 import java.util.List;
 
-public interface Parser {
-
-  ASTInContext parse(List<Token> tokens);
+public interface Parser { ASTInContext parse(List<Token> tokens);
 }
